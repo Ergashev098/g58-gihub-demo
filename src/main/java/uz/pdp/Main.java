33309503b36dbb4b52311909a073510ef2bf3bad
Umid <pubgmobile20172010@gmail.com>
2025-09-20 15:47:55 +0500

    Main methodni ichini tozoladik

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -6,23 +6,7 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
     public static void main(String[] args) {
 
-        int size = 5_000_000;
-        double[] arr = new double[size];
-        for (int i = 0; i < size; i++) {
-            arr[i] = Math.random();
-        }
 
-        int count = 0;
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            if(arr[i] > 0.2){
-                count++;
-            }
-        }
-        long end = System.currentTimeMillis();
-
-
-        System.out.println(count);
 
 //        int availableProcessorsCount = Runtime.getRuntime().availableProcessors();
 //        System.out.println(availableProcessorsCount);
